8da1514877864aea1eda139ad9b5fb22dd64d193
Robin Cornett <hello@robincornett.com>
2014-12-12 14:05:58 -0500

    refactored rss class
    First check is now full text as that is more standard. Reorganized decision process,
    and properly accounted for optional alternate feed in Send Images to RSS.

--- includes/class-displayfeaturedimagegenesis-rss.php
+++ includes/class-displayfeaturedimagegenesis-rss.php
@@ -21,15 +21,18 @@ class Display_Featured_Image_Genesis_RSS {
 		$feed_image     = $displaysetting['feed_image'];
 		$rss_option     = get_option( 'rss_use_excerpt' );
 
+		//* if the user isn't sending images to the feed, we're done
 		if ( ! $feed_image ) {
 			return;
 		}
 
-		if ( '1' === $rss_option ) {
-			add_filter( 'the_excerpt_rss', array( $this, 'add_image_to_feed' ), 1000, 1 );
-		}
-		else {
+		//* if the feed is full text, filter the content
+		if ( '0' === $rss_option ) {
 			add_filter( 'the_content_feed', array( $this, 'add_image_to_feed' ), 15 );
+		}
+		//* if the feed is summaries, filter the excerpt, not the content
+		else {
+			add_filter( 'the_excerpt_rss', array( $this, 'add_image_to_feed' ), 1000, 1 );
 		}
 
 	}
@@ -43,31 +46,48 @@ class Display_Featured_Image_Genesis_RSS {
 	 */
 	public function add_image_to_feed( $content ) {
 
+		//* if the post doesn't have a thumbnail, we're done here
 		if ( ! has_post_thumbnail() ) {
 			return $content;
 		}
 
-		$post_thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id(), 'original' );
+		//* first check: see if the featured image already exists in full in the content
+		$size = 'original';
 		if ( class_exists( 'SendImagesRSS' ) ) {
-			$post_thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id(), 'mailchimp' );
-		}
-		$image_content  = strpos( $content, 'src="' . $post_thumbnail[0] );
+			$simplify = get_option( 'sendimagesrss_simplify_feed' );
+			$alt_feed = get_option( 'sendimagesrss_alternate_feed' );
 
-		if ( false !== $image_content ) {
+			if ( ! $simplify && ( ( $alt_feed && is_feed( 'email' ) ) || ! $alt_feed ) ) {
+				$size  = 'mailchimp';
+			}
+		}
+
+		$post_thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id(), $size );
+		$image_content  = strpos( $content, 'src="' . $post_thumbnail[0] );
+		$rss_option     = get_option( 'rss_use_excerpt' );
+
+		//* if the featured image already exists in all its glory in the content, we're done here
+		if ( false !== $image_content && '0' === $rss_option ) {
 			return $content;
 		}
 
-		$rss_option = get_option( 'rss_use_excerpt' );
-		$size       = 'large';
+		//* reset size to large so we don't send huge files to the feed
+		$size = 'large';
+		if ( class_exists( 'SendImagesRSS' ) ) {
+			$simplify = get_option( 'sendimagesrss_simplify_feed' );
+			$alt_feed = get_option( 'sendimagesrss_alternate_feed' );
+			//* if the user is using Send Images to RSS, send the right images to the right feeds
+			if ( ! $simplify && ( ( $alt_feed && is_feed( 'email' ) ) || ! $alt_feed ) ) {
+				$size  = 'mailchimp';
+				$class = 'rss-mailchimp';
+			}
+		}
+
 		$align      = '';
 		$style      = 'display:block;margin:10px auto;';
 		$class      = 'rss-featured-image';
 
-		if ( class_exists( 'SendImagesRSS' ) ) {
-			$size  = 'mailchimp';
-			$class = 'rss-mailchimp';
-		}
-
+		//* if the feed output is descriptions only, change image size to thumbnail with small alignment
 		if ( '1' === $rss_option ) {
 			$size  = 'thumbnail';
 			$align = 'left';
@@ -75,11 +95,13 @@ class Display_Featured_Image_Genesis_RSS {
 			$class = 'rss-small';
 		}
 
+		//* whew. build the image!
 		$image = get_the_post_thumbnail( get_the_ID(), $size, array( 'align' => $align, 'style' => $style, 'class' => $class ) );
 
 		$content = $image . $content;
 
 		return $content;
+
 	}
 
 }
