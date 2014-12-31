2ec2fdee265fb3ac651973a312c0690f672b705e
Robin Cornett <hello@robincornett.com>
2014-12-31 17:02:14 -0500

    change button text to be the same for settings and taxonomies

--- includes/class-displayfeaturedimagegenesis-settings.php
+++ includes/class-displayfeaturedimagegenesis-settings.php
@@ -176,7 +176,7 @@ class Display_Featured_Image_Genesis_Settings {
 			echo '</div>';
 		}
 		echo '<input type="url" id="default_image_url" name="displayfeaturedimagegenesis[default]" value="' . esc_url( $this->displaysetting['default'] ) . '" />';
-		echo '<input id="upload_default_image" type="button" class="upload_default_image button" value="' . __( 'Select Default Image', 'display-featured-image-genesis' ) . '" />';
+		echo '<input id="upload_default_image" type="button" class="upload_default_image button" value="' . __( 'Select Image', 'display-featured-image-genesis' ) . '" />';
 		echo '<p class="description">' . sprintf(
 			__( 'If you would like to use a default image for the featured image, upload it here. Must be at least %1$s pixels wide.', 'display-featured-image-genesis' ),
 			absint( $item->large + 1 )
