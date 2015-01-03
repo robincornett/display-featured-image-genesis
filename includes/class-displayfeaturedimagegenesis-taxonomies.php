df532a6a20b2fdaa99ad1a52860e76d1ffcd131f
Robin Cornett <hello@robincornett.com>
2015-01-03 09:07:40 -0500

    add size to featured image description

--- includes/class-displayfeaturedimagegenesis-taxonomies.php
+++ includes/class-displayfeaturedimagegenesis-taxonomies.php
@@ -48,6 +48,7 @@ class Display_Featured_Image_Genesis_Taxonomies {
 
 		$t_id      = $term->term_id;
 		$term_meta = get_option( "taxonomy_$t_id" );
+		$large     = get_option( 'large_size_w' );
 
 		echo '<tr class="form-field">';
 			echo '<th scope="row" valign="top"><label for="term_meta[dfig_image]">' . __( 'Featured Image', 'display-featured-image-genesis' ) . '</label></th>';
@@ -62,8 +63,9 @@ class Display_Featured_Image_Genesis_Taxonomies {
 					echo '<input type="url" id="default_image_url" name="term_meta[dfig_image]" value="' . esc_url( $term_meta['dfig_image'] ) . '" style="width:200px;" />';
 					echo '<input id="upload_default_image" type="button" class="upload_default_image button" value="' . __( 'Select Image', 'display-featured-image-genesis' ) . '" />';
 					echo '<p class="description">' . sprintf(
-						__( 'Set Featured Image for %s', 'display-featured-image-genesis' ),
-						$term->name
+						__( 'Set Featured Image for %1$s. Must be at least %2$s pixels wide.', 'display-featured-image-genesis' ),
+						esc_html( $term->name ),
+						absint( $large + 1 )
 					) . '</p>';
 				echo '</td>';
 		echo '</tr>';
