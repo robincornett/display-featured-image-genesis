1aa3728e0d442957632c01b6dee8261ac95719ce
Robin Cornett <hello@robincornett.com>
2019-06-24 09:52:48 -0400

    Move author widget defaults

--- includes/widgets/displayfeaturedimagegenesis-author-widget.php
+++ includes/widgets/displayfeaturedimagegenesis-author-widget.php
@@ -38,22 +38,7 @@ class Display_Featured_Image_Genesis_Author_Widget extends WP_Widget {
 	 * @return array
 	 */
 	public function defaults() {
-		return array(
-			'title'                    => '',
-			'show_featured_image'      => 0,
-			'featured_image_alignment' => 'alignnone',
-			'featured_image_size'      => 'medium',
-			'gravatar_alignment'       => 'left',
-			'user'                     => '',
-			'show_gravatar'            => 0,
-			'size'                     => 45,
-			'author_info'              => '',
-			'bio_text'                 => '',
-			'page'                     => '',
-			'page_link_text'           => __( 'Read More', 'display-featured-image-genesis' ) . '&#x02026;',
-			'posts_link'               => 0,
-			'link_text'                => __( 'View My Blog Posts', 'display-featured-image-genesis' ),
-		);
+		return include 'fields/author-defaults.php';
 	}
 
 	/**
