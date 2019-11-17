50cb27d8f4e3dd59235fdebe9b514ba19ab05c7f
Robin Cornett <hello@robincornett.com>
2019-11-17 15:05:29 -0500

    Update dynamic term selector for widget

--- includes/js/widget-selector.js
+++ includes/js/widget-selector.js
@@ -1,26 +1,28 @@
-function term_postback( select_id, taxonomy ) {
+function term_postback ( select_id, taxonomy ) {
 	'use strict';
 	var data = {
 		action: 'widget_selector',
 		taxonomy: taxonomy
 	};
-	jQuery.post(displayfeaturedimagegenesis_ajax_object.ajax_url, data, function(response) {
+	jQuery.post( displayfeaturedimagegenesis_ajax_object.ajax_url, data, function ( response ) {
 		// Decode the data received.
-		var list = jQuery.parseJSON(response);
+		var list = jQuery.parseJSON( response );
 
 		// Keep track of what was previously selected
-		var control = jQuery('#' + select_id);
+		var control = jQuery( '#' + select_id );
 		var old_value = control.val();
 
 		// Clear out the old options, build up the new
 		control.empty();
-		jQuery.each(list, function(key, value) {
-			var new_option = jQuery('<option></option>')
-				.attr('value', key).text(value);
-			if (value == old_value) {
-				new_option.attr('selected', true);
+		jQuery.each( list, function ( key, value ) {
+			var new_option = jQuery( '<option />' )
+				.val( key ).text( value );
+			control.val( '' );
+			if ( value === old_value ) {
+				control.val( old_value );
 			}
-			control.append(new_option);
-		});
-	});
+			var method = !key ? 'prepend' : 'append';
+			control[ method ]( new_option );
+		} );
+	} );
 }
